4feca299a6f99e1da34d7c3e7f2ce199be645c95
weikent <wei.shijian@gmail.com>
2013-12-22 10:51:42 +0800

    把这个下载漫画的C#程序放到这里把

---
+++ forComic/forComic/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace forComic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_url = new System.Windows.Forms.TextBox();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_volume = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox_savePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始下载";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_url
+            // 
+            this.textBox_url.Location = new System.Drawing.Point(99, 12);
+            this.textBox_url.Name = "textBox_url";
+            this.textBox_url.Size = new System.Drawing.Size(406, 21);
+            this.textBox_url.TabIndex = 1;
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(99, 93);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(116, 21);
+            this.textBox_name.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "下载路径：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "漫画名称：";
+            // 
+            // textBox_volume
+            // 
+            this.textBox_volume.Location = new System.Drawing.Point(574, 12);
+            this.textBox_volume.Name = "textBox_volume";
+            this.textBox_volume.Size = new System.Drawing.Size(70, 21);
+            this.textBox_volume.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(551, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "卷";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(525, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 21);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "指定下载位置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox_savePath
+            // 
+            this.textBox_savePath.Location = new System.Drawing.Point(30, 52);
+            this.textBox_savePath.Name = "textBox_savePath";
+            this.textBox_savePath.Size = new System.Drawing.Size(475, 21);
+            this.textBox_savePath.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 248);
+            this.Controls.Add(this.textBox_savePath);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_volume);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.textBox_url);
+            this.Controls.Add(this.button1);
+            this.Name = "下载漫画";
+            this.Text = "下载漫画";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_url;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_volume;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox_savePath;
+    }
+}
+
---
+++ forComic/forComic/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+namespace forComic
+{
+    public partial class Form1 : Form
+    {   WebResponse response = null;
+        StreamReader reader = null;
+        string ComicUrl = string.Empty;
+        string serviceUrl = "http://comic.131.com";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ComicUrl = textBox_url.Text;
+            bool Next = false;
+            int j = Convert.ToInt16(textBox_volume.Text);//start Volume
+
+            do
+            {
+                Directory.CreateDirectory(textBox_savePath.Text + "\\Chapter" + j);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(ComicUrl);
+                request.Method = "GET";
+                response = request.GetResponse();
+                reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                string urlContent = reader.ReadToEnd();
+
+                int maxNum = maxNumberOfPage(urlContent);
+
+                for (int i = 1; i < maxNum + 1; i++)
+                {
+                    request = (HttpWebRequest)WebRequest.Create(ComicUrl.Replace("1.html", i + ".html"));
+                    request.Method = "GET";
+                    response = request.GetResponse();
+                    reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                    string currentVolumeContent = reader.ReadToEnd();
+
+
+                    int a = currentVolumeContent.IndexOf("http://131js.131.com/channel/comic/img.js?");
+                    a = a + "http://131js.131.com/channel/comic/img.js?".Length + 4;
+                    string suffix = string.Empty;
+                    int b = currentVolumeContent.IndexOf(".jpg\"><");
+                    suffix = ".jpg";
+                    if (b == -1)
+                    {
+                        b = currentVolumeContent.IndexOf(".png\"><");
+                        suffix = ".png";
+                    }
+
+                    string url = currentVolumeContent.Substring(a, b - a);
+
+                    url += suffix;
+
+                    //downfile(url, "d:\\aa.jpg");
+
+                    down(url, "d:\\" + textBox_name.Text + "\\Chapter" + j + "\\Chapter" + i + ".jpg");
+                }
+
+                hasNext(urlContent);
+                j++;
+            }
+            while (!Next);
+        }
+
+
+        public void down(string url, string localpath)
+        {
+            string ural = url;
+            string filepath = localpath;
+            WebClient mywebclient = new WebClient();
+            mywebclient.DownloadFile(ural, filepath);
+            
+        }
+
+        public bool hasNext(string urlContent)
+        {
+            string result = urlContent;
+
+            int a = result.IndexOf("观看同类漫画");
+            
+            //下一话：<a href="/content/2066/151303/1.html">第216话</a></span>
+            a = result.IndexOf("下一话：<a href=");
+            int b = result.Substring(a + "下一话：<a href=".Length).IndexOf("\">");
+            string NextUrl = result.Substring(a + "下一话：<a href=".Length).Substring(1, b - 1);
+
+            ComicUrl = serviceUrl + NextUrl;
+
+            if (a == -1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int maxNumberOfPage(string urlContent)
+        {
+            string result = urlContent;
+            int a = result.IndexOf("当前");
+
+            //<p>当前 <em>2</em>/20</p>
+            string b = result.Substring(a);
+            a = b.IndexOf(">/");
+            b = b.Substring(a, 10);
+
+            string num = b.Substring(2,b.IndexOf("</")-2);
+
+
+            return Convert.ToInt16(num);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            if (folderDlg.ShowDialog() == DialogResult.OK)
+            {
+                textBox_savePath.Text = folderDlg.SelectedPath;
+            }
+        }
+    }
+}
