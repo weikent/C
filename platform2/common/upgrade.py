f88006573a29b19695998804e12110222944663d
weikent <wei.shijian@gmail.com>
2017-04-07 14:53:49 +0800

    use python3

--- platform2/common/upgrade.py
+++ platform2/common/upgrade.py
@@ -10,6 +10,12 @@ import telnetlib
 import json
 import os
 
+
+
+
+TIME_OUT = 5
+
+
 def upgrade():
     userName = ""
     password = ""
@@ -27,47 +33,69 @@ def upgrade():
         a = json.loads(j)
         ip = a["ip"]
         commands = a["command"]
-        print "you want to upgrade those ip(s):\n"
+        print("you want to upgrade those ip(s):\n")
         for item in ip:
-            print item
-        confirmation = raw_input("please make a confirmation (Y/N):")
+            print (item)
+
+        confirmation = input("please make a confirmation (Y/N):")
         if confirmation != "Y":
             exit(1)
 
-        print "you want to use those command(s) to upgrade:\n"
+        print ("you want to use those command(s) to upgrade:\n")
         for item in commands:
-            print item
-        confirmation = raw_input("please make a confirmation (Y/N):")
+            print (item)
+        confirmation = input("please make a confirmation (Y/N):")
         if confirmation != "Y":
             exit(1)
 
     else:
-        print "please create %s first, using -g parameter." %("info.json")
+        print ("please create %s first, using -g parameter." %("info.json"))
         exit(1)
 
     # step 2 get username and password
-    username = raw_input("please input username(used for telnet):")
-    print "please input password(used for telnet):"
+    username = input("please input username(used for telnet):")
+    print ("please input password(used for telnet):")
     password = getpass.getpass()
-
 
     # step 3 telnet
     for item in ip:
-        tn = telnetlib.Telnet(HOST)
+        print ("connecting", item)
+        try:
+            tn = telnetlib.Telnet(item,23, TIME_OUT)
+        except:
+            info = sys.exc_info()
+            print ("telnet failed, the reason is :", info[1])
+            exit(0)
         tn.set_debuglevel(0)
-        tn.read_until("login: ")
-        tn.write(user + "\n")
-        if password:
-            tn.read_until("Password: ")
-            tn.write(password + "\n")
+        tn.read_until(b'login: ')
+        tn.write((username + " \n").encode(encoding="utf-8"))
+        tn.read_until(b'Password: ')
+        tn.write((password + "\n").encode(encoding="utf-8"))
+
+        try:
+            ret = tn.read_until(b"login: ", TIME_OUT)
+            print ("ret:", ret.decode())
+            print (type(ret))
+            if b'login: ' in ret:
+                print ("the username and password you input may be wrong, please check and re-run this app again.")
+                exit(1)
+            else:
+                for item_command in commands:
+                    tn.write((item_command + " \n").encode(encoding="utf-8"))
+                    ret = tn.read_until(b"#", TIME_OUT)
+                    print (ret.decode())
+
+        except EOFError:
+            print ('no data can be readed')
+            exit(1)
 
 
 def usage():
     '''
     '''
-    print "\nUSAGE: python upgrade.py -f  show help file."
-    print "                         -u  start upgrade."
-    print "                         -g  generate json file."
+    print ("\nUSAGE: python upgrade.py -f  show help file.")
+    print ("                         -u  start upgrade.")
+    print ("                         -g  generate json file.")
 
 def generate():
     dic = {}
